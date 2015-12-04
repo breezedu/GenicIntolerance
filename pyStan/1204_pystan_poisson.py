4196a78c6397901cc7752f2fd32ae2db71b3cced
breezedu <breeze.du@gmail.com>
2015-12-04 15:21:56 -0500

    add cluster output figures

---
+++ pyStan/1204_pystan_poisson.py
@@ -0,0 +1,75 @@
+import matplotlib
+matplotlib.use('Agg')
+import pystan
+import numpy as np
+#from pystan.external.pymc.plots import traceplot
+import matplotlib.pyplot as mp
+
+#import matplotlib
+#import matplotlib.pyplot
+
+stan_code = """
+data {
+    int<lower=0> J; // number of obs
+    int<lower=0> N; // number of gene level
+    int<lower=1, upper=J> gene[N]; // number of obs
+
+    row_vector[N] x[J]; x// estimated treatment effects
+    int<lower=0> sigma[N]; // s.e. of effect estimates
+}
+
+parameters {
+    row_vector[J] a;
+    real<lower=0> sigma_a;
+    real<lower=0> sigma_epsilon;
+    real beta;
+    row_vector[N] epsilon_raw[J];
+}
+transformed parameters {
+    row_vector[N] lambda[J];
+    row_vector[N] epsilon[J];
+    for (i in 1:N)
+    	epsilon[i] <- sigma_epsilon * epsilon_raw[i];
+    for (i in 1:N)
+	lambda[i] <- beta*x[i] + a[gene[i]] + epsilon[i]; 
+}
+
+model {
+	
+	beta ~ normal(0, 10);
+	a ~ normal(0, sigma_a);
+	epsilon_raw ~ normal(0, 1);
+	y ~ poisson_log(lambda); 
+}
+"""
+
+schools_dat = {'J': 8,
+               'y': [28,  8, -3,  7, -1,  1, 18, 12],
+               'sigma': [15, 10, 16, 11,  9, 11, 10, 18]}
+
+fit = pystan.stan(model_code=schools_code, data=schools_dat,
+                  iter=1000, chains=4)
+
+print(fit)
+
+eta = fit.extract(permuted=True)['eta']
+np.mean(eta, axis=0)
+
+# if matplotlib is installed (optional, not required), a visual summary and
+# traceplot are available
+
+print("plot figure")
+
+fit.plot()
+mp.savefig("StanPlot.png")
+
+#mp.figure(figsize=(8,6))
+#mp.plot(fit)
+#mp.savefig('fit.png')
+#mp.close()
+
+#mp.plot(fit)
+#mp.show()
+
+#traceplot(fit)
+
