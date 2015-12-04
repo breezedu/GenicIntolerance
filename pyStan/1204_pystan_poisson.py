50444c21aedf4a7dee38349bfd1187aab44edaf6
breezedu <breeze.du@gmail.com>
2015-12-04 15:23:06 -0500

    update 1204 pystan poisson script

--- pyStan/1204_pystan_poisson.py
+++ pyStan/1204_pystan_poisson.py
@@ -43,6 +43,8 @@ model {
 }
 """
 
+
+## Have to read data from txt document:
 schools_dat = {'J': 8,
                'y': [28,  8, -3,  7, -1,  1, 18, 12],
                'sigma': [15, 10, 16, 11,  9, 11, 10, 18]}
