d6fe79063e7aa01454734e5af1d73c4cbd1e6001
breezedu <breeze.du@gmail.com>
2015-12-28 16:30:53 -0500

    plot power loss of homozygosity R code

---
+++ HomozygosityMutations/plot.power.loss.of.homozygosity.R
@@ -0,0 +1,6 @@
+pwr.strat<-c(0.01,0.07,0.23,0.52,0.79,.97)
+pwr.hom<-c(0.07,0.15,0.34,0.55,.8,.99)
+bta<-c(1,.85,.65,.5,.35,.15)
+plot(1/bta,pwr.strat,type="l",col="red",xlab="1/beta",ylab="power",lwd=2,ylim=c(0,1))
+lines(1/bta,pwr.hom,lwd=2)
+legend(4,.45,c("Homogeneous","Stratified"),fill=c("black","red"))
