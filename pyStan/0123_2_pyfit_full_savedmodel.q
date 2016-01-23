004c9738727eaa9f1e9519be1a5a3fdfac2486c1
breezedu <breeze.du@gmail.com>
2016-01-23 12:12:27 -0500

    update 0123 2nd pystan scripts

--- pyStan/0123_2_pyfit_full_savedmodel.q
+++ pyStan/0123_2_pyfit_full_savedmodel.q
@@ -3,8 +3,8 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=breeze.du@gmail.com
 #SBATCH -c 16
-#SBATCH --mem-per-cpu=5G
-#SBATCH --nodes=4
+#SBATCH --mem-per-cpu=8G
+#SBATCH --nodes=8
 #SBATCH --job-name=PyStanSavedModel
 
 /dscrhome/gd44/jeffapps/bin/python2.7 0121_pystan_AllExon_SavedModel.py
--- pyStan/0123_2_pystan_AllExon_SavedModel.py
+++ pyStan/0123_2_pystan_AllExon_SavedModel.py
@@ -178,7 +178,7 @@ new_model = load('pyfitfull_model')
 print("\n fit pystan model")
 
 ## fit=pystan.stan(model_code="pystancode.stan", data=Mtable, iter=1000, chains=4)
-fit = new_model.sampling(Mtable, iter = 1000, warmup= 800, thin=2, chains=4)
+fit = new_model.sampling(Mtable, iter = 3000, warmup= 2800, thin=2, chains=4)
 
 ####################################################
 #extract the result
@@ -197,7 +197,7 @@ print(fit)
 print("plot fit figure")
 
 fit.plot()
-mp.savefig("0121_PyStan_Exon_Plot.png")
+mp.savefig("0123_2nd_PyStan_Exon_Plot.png")
 
 
 ####################################################
