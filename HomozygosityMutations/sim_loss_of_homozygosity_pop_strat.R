3b337514d93a296987caaccfffc71574e85caa1e
breezedu <breeze.du@gmail.com>
2016-02-22 11:27:49 -0500

    update sim_loss_of_homozygosity_pop_strat.1.R code

--- HomozygosityMutations/sim_loss_of_homozygosity_pop_strat.R
+++ HomozygosityMutations/sim_loss_of_homozygosity_pop_strat.R
@@ -11,11 +11,11 @@
 
 
 
-nsites<-10          #number of polymorphic qualifying sites in gene
-np1<-3000           #sample size popn1
-np2<-3000           #sample size popn2
+nsites<-10          # number of polymorphic qualifying sites in gene
+np1<-3000           # sample size popn1
+np2<-3000           # sample size popn2
 n<-np1+np2
-p.upper<-0.01       #upper bound on qualifying variants
+p.upper<-0.01       # upper bound on qualifying variants
 b.v<-.95            # baseline viability (probability of being viable give zero or one affected gene copies)
 bta<-1/10           # relative risk of viability given 2 affected copies versus baseline
 nsim<-100
