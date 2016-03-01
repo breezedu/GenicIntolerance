35abfd74e25a0141219a7b8ebde14bd64ab36ec5
breezedu <breeze.du@gmail.com>
2016-03-01 16:47:19 -0500

    add gene.java class

---
+++ HomozygosityMutations/gene.java
@@ -0,0 +1,17 @@
+package coursera_java_duke;
+
+/**********
+ * create an object of gene
+ * each gene has three parmaters: name, ones, and twos.
+ * 
+ * @author Jeff
+ *
+ */
+class gene{
+	
+	String name = "";
+	int zeros = 0;
+	int ones = 0;
+	int twos = 0;
+	
+}
