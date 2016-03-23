720ca3397aad9abe4d31572090d341bf0ed53724
breezedu <breeze.du@gmail.com>
2016-03-23 00:37:46 -0400

    update D0322 saveGeneNames_inALSdata.java

--- HomozygosityMutations/D0322_saveGeneNames_in_ALSdata.java
+++ HomozygosityMutations/D0322_saveGeneNames_in_ALSdata.java
@@ -12,85 +12,87 @@ public class D0322_saveGeneNames_in_ALSdata {
 	public static void main(String[] args) throws IOException{
 		
 		//1st readin text file from local dick.
-				//D:\PhD\LizDeidentified_151002\LizDeidentified_151002
-				String routine = "D:/PhD/LizDeidentified_151002/LizDeidentified_151002/";
+		//D:\PhD\LizDeidentified_151002\LizDeidentified_151002
+		String routine = "D:/PhD/LizDeidentified_151002/LizDeidentified_151002/";
 				
-				//initial names for each documents, there are 12 of them together.
-				String[] doc = new String[12];
+		//initial names for each documents, there are 12 of them together.
+		String[] doc = new String[12];
 				
-				//only two documents for test code:
-				//doc[0] = "gene_samp_matrix_high_LOF_het";
-				//doc[1] = "gene_samp_matrix_high_LOF_hom";
+		//only two documents for test code:
+		//doc[0] = "gene_samp_matrix_high_LOF_het";
+		//doc[1] = "gene_samp_matrix_high_LOF_hom";
+			
+		/**/
+		doc[0] = "gene_samp_matrix_high_LOF_het";
+		doc[1] = "gene_samp_matrix_high_LOF_hom";
+		doc[2] = "gene_samp_matrix_high_not_benign_het";
+		doc[3] = "gene_samp_matrix_high_not_benign_hom";
+			
+		doc[4] = "gene_samp_matrix_high_not_syn_het";
+		doc[5] = "gene_samp_matrix_high_not_syn_hom";
+		doc[6] = "gene_samp_matrix_low_LOF_het";
+		doc[7] = "gene_samp_matrix_low_LOF_hom";
 				
-				/**/
-				doc[0] = "gene_samp_matrix_high_LOF_het";
-				doc[1] = "gene_samp_matrix_high_LOF_hom";
-				doc[2] = "gene_samp_matrix_high_not_benign_het";
-				doc[3] = "gene_samp_matrix_high_not_benign_hom";
-				
-				doc[4] = "gene_samp_matrix_high_not_syn_het";
-				doc[5] = "gene_samp_matrix_high_not_syn_hom";
-				doc[6] = "gene_samp_matrix_low_LOF_het";
-				doc[7] = "gene_samp_matrix_low_LOF_hom";
-				
-				doc[8] = "gene_samp_matrix_low_not_benign_het";
-				doc[9] = "gene_samp_matrix_low_not_benign_hom";
-				doc[10] = "gene_samp_matrix_low_not_syn_het";
-				doc[11] = "gene_samp_matrix_low_not_syn_hom";
-				/*****/
+		doc[8] = "gene_samp_matrix_low_not_benign_het";
+		doc[9] = "gene_samp_matrix_low_not_benign_hom";
+		doc[10] = "gene_samp_matrix_low_not_syn_het";
+		doc[11] = "gene_samp_matrix_low_not_syn_hom";
+		/*****/
 				
 				
 				
-				//2nd, innate an arrayList to store all the names of genes in ALS datafile.
-				ArrayList<String> geneNames = new ArrayList<String>();
-				System.out.println("Step two:");
+		//2nd, innate an arrayList to store all the names of genes in ALS datafile.
+		ArrayList<String> geneNames = new ArrayList<String>();
+		System.out.println("Step two:");
 				
 
-				//for each document, check how many genes, save them to geneNames arrayList, also check mismatches between these gene names.
-				for(int i=0; i<doc.length; i++){
+		//for each document, check how many genes, save them to geneNames arrayList, also check mismatches between these gene names.
+		for(int i=0; i<doc.length; i++){
 					
-					//append .txt to each document.
-					doc[i] += ".txt";
+			//append .txt to each document.
+			doc[i] += ".txt";
 					
-					//use a Scanner to 'scan' the document.
-					Scanner read_in = new Scanner( new File(routine + doc[i]));
-					
+			//use a Scanner to 'scan' the document.
+			Scanner read_in = new Scanner( new File(routine + doc[i]));
+			
 
-					//check each headline of each doc, to extract the gene names.
-					geneNames = getNamesFromEachDoc(read_in, doc[i], geneNames);
+			//check each headline of each doc, to extract the gene names.
+			geneNames = getNamesFromEachDoc(read_in, doc[i], geneNames);
 					
-					//close the read_in doc.
-					read_in.close();
+			//close the read_in doc.
+			read_in.close();
 				
-				}//end for loop;
-				
-				
-				
-				//get gencode.v24.basic.annotation.gftf document from: ftp://ftp.sanger.ac.uk/pub/gencode/Gencode_human/release_24/
+		}//end for loop;
 				
 				
 				
+		//get gencode.v24.basic.annotation.gftf document from: ftp://ftp.sanger.ac.uk/pub/gencode/Gencode_human/release_24/
+		//this part will leave to D0322_moreInfoAboutGenes.java part;		
 				
-				//save the gene names to a txt document
-				File output = new File(routine + "/gene_names.txt");
-				BufferedWriter out_Writer = new BufferedWriter(new FileWriter(output));
 				
-				//write every string in the geneNames arrayList to the output file
-				for(int i=0; i<geneNames.size(); i++){
+				
+		//save the gene names to a txt document
+		File output = new File(routine + "/gene_names.txt");
+		BufferedWriter out_Writer = new BufferedWriter(new FileWriter(output));
+			
+		//write every string in the geneNames arrayList to the output file
+		for(int i=0; i<geneNames.size(); i++){
 					
-					out_Writer.write(geneNames.get(i) + "\t");
-				}
+			out_Writer.write(geneNames.get(i) + "\t");
+		}
 				
-				out_Writer.write("\n");
+		out_Writer.write("\n");
 		
 				
-				out_Writer.close();
+		out_Writer.close();
 				
 	}//end main();
 
 	
 	/******************
-	 * 
+	 * pass the file reader and the geneNames ArrayList to this method;
+	 * check the headline of each file, attract gene names, and compare these names and sequence to the name array we already have;
+	 * return the geneNames;
 	 * @param read_in
 	 * @param docName
 	 * @param geneNames
