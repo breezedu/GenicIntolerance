662cd07d71e46da9bd0229c59f08140ae5855e68
breezedu <breeze.du@gmail.com>
2016-04-22 17:55:16 -0400

    add 0420 frame scracth

---
+++ HomozygosityMutations/D0420_SAMD11_Pai2g.java
@@ -0,0 +1,12 @@
+package coursera_java_duke;
+
+public class D0420_SAMD11_Pai2g {
+	
+	/***************
+	 * Get all the mutation site within gene SAMD11, also delete those not on any Exons; 
+	 * get the Allele count over population size AC/60,706;
+	 * since the real number is too small, so get the log(AC/60,706)
+	 * 
+	 */
+
+}
