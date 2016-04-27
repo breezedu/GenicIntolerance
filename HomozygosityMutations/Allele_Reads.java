e015a91902b1b93bf2327b73d910e50be896d471
breezedu <breeze.du@gmail.com>
2016-04-27 12:02:38 -0400

    add Allele_reads.java object class

---
+++ HomozygosityMutations/Allele_Reads.java
@@ -0,0 +1,15 @@
+package coursera_java_duke;
+
+/**********
+ * a naive object for allele count reads
+ * 
+ * @author Jeff
+ *
+ */
+public class Allele_Reads {
+
+	public String chr;			//chromosome 
+	public int pos;				//position of the read
+	public int allele_count;	//AC
+	public String Pol;			//polyphen score
+}
