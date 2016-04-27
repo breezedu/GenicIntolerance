d9d0d968c4e021fe04719e3482a8c35ebe3394a3
breezedu <breeze.du@gmail.com>
2016-04-27 12:01:50 -0400

    add Exon_frame.java

---
+++ HomozygosityMutations/Exon_frame.java
@@ -0,0 +1,20 @@
+package coursera_java_duke;
+
+/************
+ * a draft of exon objects
+ * will update private-exon_inf.set() functions later;
+ * 
+ * @author Jeff
+ * 04/27/2016
+ */
+public class Exon_frame {
+	
+	public String Chr;
+	public String gene_name;
+	public int gene_start;
+	public int gene_end;
+	public String exon_name;
+	public int exon_start;
+	public int exon_end;
+
+}
